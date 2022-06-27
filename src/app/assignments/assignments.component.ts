98b6cbc319ec7572c707c96bf8c4e7f1b3dcda0f
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-06-27 17:13:47 -0500

    Create and use service for data

---
+++ src/app/assignments/assignments.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { AssignmentsService } from '../assignments.service';
+import { Assignment } from './assignment.model';
+
+@Component({
+  selector: 'app-assignments',
+  templateUrl: './assignments.component.html',
+  styleUrls: ['./assignments.component.css']
+})
+export class AssignmentsComponent implements OnInit {
+  @Output() assignments : Assignment[];
+
+  constructor(private assignmentsService : AssignmentsService) { }
+
+  ngOnInit(): void {
+    this.assignments = this.assignmentsService.getAssignments()
+  }
+
+}
