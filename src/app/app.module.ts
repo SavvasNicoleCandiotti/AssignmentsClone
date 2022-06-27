67c6258f343c750f44e74c94d958668eed0023af
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-06-27 17:16:09 -0500

    Add declarations for assignment & course components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { AssignmentComponent } from './assignments/assignment/assignment.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseComponent } from './courses/course/course.component';
   // import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentsComponent,
+    AssignmentComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
