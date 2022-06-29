7165de906b2d71e019b73660eef094af81db5536
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-06-29 09:00:04 -0500

    Create routes for assignments, assignment and home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssignmentDetailComponent } from './assignments/assignment-detail/assignment-detail.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'assignments', component: AssignmentsComponent},
+  {path: 'assignments/:id', component: AssignmentDetailComponent},
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
