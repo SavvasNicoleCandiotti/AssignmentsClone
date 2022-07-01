0147fce70c0acca8d69560c166de47fb81ac6f18
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-07-01 00:48:28 -0500

    Add property status and handle loading status

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -19,25 +19,36 @@ export class AssignmentsComponent implements OnInit {
     description: string
   }[] = []
 
+  public status : string = "idle"
 
   constructor(private assignmentsService : AssignmentsService) {
   }
 
   ngOnInit(): void {
-    if(this.assignmentsService.initialFetch){
+
+    this.assignmentsService.fetchEvent
+      .subscribe(status => this.status = status);
+    
+    this.status = this.assignmentsService.getStatus()
+    if(this.status === "success"){
         this.assignmentsArray = this.assignmentsService.getAssignments()
           console.log("Didn't fetch")
-      }else{
+      }else if(this.status === "idle"){
         this.getAllAssignments()
-        console.log("Fetched")
       }
   }
 
   getAllAssignments(){
-    this.assignmentsService.fetchAllAssignments().subscribe((r)=>{
-      this.assignmentsService.setAssignments(r)
-      this.assignmentsArray = this.assignmentsService.getAssignments()
-      this.assignmentsService.initialFetch = true 
-    })
+    this.assignmentsService.setStatus("loading")
+    this.assignmentsService.fetchEvent.emit("loading")
+    
+    setTimeout(
+      () => this.assignmentsService.fetchAllAssignments().subscribe((r)=>{
+        this.assignmentsService.setAssignments(r)
+        this.assignmentsArray = this.assignmentsService.getAssignments()
+        this.assignmentsService.setStatus("success")
+        this.assignmentsService.fetchEvent.emit("success")
+        console.log("Fetched")
+      }), 1000) 
   }
 }
