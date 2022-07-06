11a06dccacfb6922c37aa54e469faf223b90b31a
SavvasNicoleCandiotti <nicole.candiotti@savvas.com>
2022-07-06 10:37:59 -0400

    Create modal formgroup logic

--- src/app/assignments/create-assignment-modal/create-assignment-modal.component.ts
+++ src/app/assignments/create-assignment-modal/create-assignment-modal.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Output, EventEmitter, Input, enableProdMode } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import {AssignmentInterface} from "../AssignmentInterface"
+import { AssignmentsService } from '../assignments.service';
+
+
 
 @Component({
   selector: 'app-create-assignment-modal',
@@ -7,31 +12,49 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-assignment-modal.component.css']
 })
 export class CreateAssignmentModalComponent implements OnInit {
-  @Input() program_id?:number; //passing program id as an input from program detail component
+  program_id:number =  parseInt(this.route.snapshot.params['id']) //taking the id from the route parameters and assigning it to variable
   @Input() showModal:boolean
+  randomTestNumber = 5
   @Output() modalBtnClick = new EventEmitter()
+  @Output() onAddAssignment: EventEmitter<AssignmentInterface> = new EventEmitter()
+
+
+
   modalForm = new FormGroup({
     title: new FormControl('', Validators.required),
     description: new FormControl('', [Validators.required, Validators.maxLength(500)]),
+    // does this have to be a form control since there's no user input? doesn't seem to work w/o form control
     program_id: new FormControl(this.program_id)
+    // randomTestNumber: new FormControl(this.randomTestNumber),
   });
   charsRemaining:number=500
 
 
-  constructor() { }
+  constructor(private assignmentsService: AssignmentsService,
+  private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
+  //click event for closing modal X button
   onClick(){
     this.modalBtnClick.emit()
   }
 
-  submitModalForm(e:Event){
+  submitModalForm(e:Event, value:AssignmentInterface){
     e.preventDefault()
-    console.log("form submitted")
+    // this neeeds to take in an assignment
+    this.onAddAssignment.emit({title: value.title, description: value.description, program_id: this.program_id})
+
+    console.log(this.modalForm.value)
     console.log(this.program_id)
+    console.log("form submitted")
+    // reset values once form is submitted - is there a reset method on forms?
+    // this.modalForm.reset()
+
+    // closes modal once form is submitted
      this.modalBtnClick.emit()
   }
+  // sets length for character limit
   setLength(event){
  this.charsRemaining = 500 - event.length}
   }
