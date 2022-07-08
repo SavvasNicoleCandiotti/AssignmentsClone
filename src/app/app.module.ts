f9a3b380e229401de9ea85857a5a90168072feaa
SavvasNicoleCandiotti <nicole.candiotti@savvas.com>
2022-07-08 15:36:20 -0400

    added forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
@@ -52,12 +52,12 @@ import { CreateCourseAssignmentModalComponent } from './course-assignments/creat
     ProgramAssignmentsComponent,
     ProgramAssignmentComponent,
     ProgramAssignmentDetailComponent,
-    EditCourseAssignmentModalComponent, 
+    EditCourseAssignmentModalComponent,
     CreateCourseAssignmentModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, HttpClientModule, ReactiveFormsModule, FontAwesomeModule
+    AppRoutingModule, HttpClientModule, ReactiveFormsModule, FontAwesomeModule, FormsModule
 
   ],
   providers: [],
