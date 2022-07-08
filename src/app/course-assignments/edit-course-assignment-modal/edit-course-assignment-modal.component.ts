d26b8917673960f774ae740f49b222eddf613ea1
SavvasNicoleCandiotti <nicole.candiotti@savvas.com>
2022-07-08 15:37:16 -0400

    edit modal form html and submit functionality

--- src/app/course-assignments/edit-course-assignment-modal/edit-course-assignment-modal.component.ts
+++ src/app/course-assignments/edit-course-assignment-modal/edit-course-assignment-modal.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CourseAssignmentsService } from 'src/app/services/course-assignments.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-edit-course-assignment-modal',
@@ -8,17 +11,18 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./edit-course-assignment-modal.component.css']
 })
 export class EditCourseAssignmentModalComponent implements OnInit {
-  @Input() courseAssignment
+  @Input() courseAssignment = this.courseAssignmentsService.getCourseAssignment(parseInt(this.route.snapshot.params['id']))
   @Output() closeEditModal = new EventEmitter()
   faXmark=faXmark
   editForm = new FormGroup ({
-    title: new FormControl(''),
-    description: new FormControl(''),
-    assignedOn: new FormControl(''),
-    dueOn: new FormControl(''),
+    id: new FormControl(this.courseAssignment.id),
+    assignedOn: new FormControl(this.courseAssignment.assignedOn),
+    dueOn: new FormControl(this.courseAssignment.dueOn),
   })
 
-  constructor() { }
+  constructor(private courseAssignmentsService: CourseAssignmentsService,
+    private route: ActivatedRoute,
+    private router : Router) { }
 
   ngOnInit(): void {
 
@@ -26,6 +30,27 @@ export class EditCourseAssignmentModalComponent implements OnInit {
   handleCloseEditModal(){
     this.closeEditModal.emit()
   }
+  submitEditForm(value){
+    this.courseAssignmentsService.patchAssignment(value).subscribe(assignment => {
+      this.courseAssignmentsService.updateCourseAssignment(assignment)
 
+      //the event needs to be emitted inside this code block, it doesn't work below it
+      this.courseAssignmentsService.updateEvent.emit()
+    });
+    // reset values once form is submitted
+    this.editForm.reset()
+    // closes modal once form is submitted
+    this.closeEditModal.emit()
+
+  }
+
+  handleDelete(assignment){
+    console.log(assignment)
+    this.courseAssignmentsService.deleteAssignment(assignment).subscribe(assignment =>{
+      this.courseAssignmentsService.removeAssignmentFromArray(assignment)
+    }) //and then filter it out here by subscribing to service event to be  created
+    alert(`successfully deleted ${assignment.title} for ${assignment.course_name}`)
+     this.router.navigate([''])
+  }
 
 }
