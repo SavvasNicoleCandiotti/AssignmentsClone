aecf38e7752f0d131283aa284f1b0eb584c92cfc
SavvasNicoleCandiotti <nicole.candiotti@savvas.com>
2022-07-10 18:16:27 -0400

    fixed html formatting w prettier

--- src/app/course-assignments/create-course-assignment-modal/create-course-assignment-modal.component.ts
+++ src/app/course-assignments/create-course-assignment-modal/create-course-assignment-modal.component.ts
@@ -13,86 +13,92 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-create-course-assignment-modal',
   templateUrl: './create-course-assignment-modal.component.html',
-  styleUrls: ['./create-course-assignment-modal.component.css']
+  styleUrls: ['./create-course-assignment-modal.component.css'],
 })
 export class CreateCourseAssignmentModalComponent implements OnInit {
-  @Input() assignment : AssignmentInterface = this.assignmentsService.getSelectedAssignment()
-  @Input() showModal : boolean = false
-  randomTestNumber = 5
-  @Output() modalBtnClick = new EventEmitter()
-  @Output() onAddAssignment: EventEmitter<CourseAssignmentInterface> = new EventEmitter()
-  faXmark = faXmark
+  @Input() assignment: AssignmentInterface =
+    this.assignmentsService.getSelectedAssignment();
+  @Input() showModal: boolean = false;
+  randomTestNumber = 5;
+  @Output() modalBtnClick = new EventEmitter();
+  @Output() onAddAssignment: EventEmitter<CourseAssignmentInterface> =
+    new EventEmitter();
+  faXmark = faXmark;
 
-  coursesArray : {} = this.coursesService.getCourses()
+  coursesArray: {} = this.coursesService.getCourses();
 
   courseAssignmentForm = new FormGroup({
     course_id: new FormControl(null, Validators.required),
     assignment_id: new FormControl(this.assignment.id, Validators.required),
     assignedOn: new FormControl(new Date(), Validators.required),
-    dueOn: new FormControl(new Date(), Validators.required)
+    dueOn: new FormControl(new Date(), Validators.required),
   });
 
-  charsRemaining:number=500
+  charsRemaining: number = 500;
 
   constructor(
-    private courseAssignmentsService : CourseAssignmentsService,
-    private assignmentsService : AssignmentsService,
-    private coursesService : CoursesService,
-    private httpService : HttpService,
-    private route : ActivatedRoute
-  ) {
-   }
+    private courseAssignmentsService: CourseAssignmentsService,
+    private assignmentsService: AssignmentsService,
+    private coursesService: CoursesService,
+    private httpService: HttpService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.courseAssignmentsService.toggleModalEvent.subscribe((boolean) => console.log("hi"))
-    if(this.coursesService.getCoursesStatus() === "idle"){
-      this.getCourses()
-    }else{
-      this.coursesArray = this.coursesService.getCourses()
+    this.courseAssignmentsService.toggleModalEvent.subscribe((boolean) =>
+      console.log('hi')
+    );
+    if (this.coursesService.getCoursesStatus() === 'idle') {
+      this.getCourses();
+    } else {
+      this.coursesArray = this.coursesService.getCourses();
     }
   }
 
-  onClick(){
-    this.courseAssignmentsService.toggleModalEvent.emit(false)
-    this.modalBtnClick.emit()
+  onClick() {
+    this.courseAssignmentsService.toggleModalEvent.emit(false);
+    this.modalBtnClick.emit();
   }
 
-  submitModalForm(value : CourseAssignmentInputInterface ){
+  submitModalForm(value: CourseAssignmentInputInterface) {
     // this neeeds to take in an assignment
     // this.onAddAssignment.emit(value)
-    this.courseAssignmentsService.postAssignment(value)
-    .subscribe((courseAssignment : CourseAssignmentInterface) => {
-      this.courseAssignmentsService.addCourseAssignment(courseAssignment)
-      //the event needs to be emitted inside this code block, it doesn't work below it
-      this.httpService.postEvent.emit()
-    });
+    this.courseAssignmentsService
+      .postAssignment(value)
+      .subscribe((courseAssignment: CourseAssignmentInterface) => {
+        this.courseAssignmentsService.addCourseAssignment(courseAssignment);
+        //the event needs to be emitted inside this code block, it doesn't work below it
+        this.httpService.postEvent.emit();
+      });
 
     // reset values once form is submitted - is there a reset method on forms?
-    this.courseAssignmentForm.reset()
+    this.courseAssignmentForm.reset();
 
     // closes modal once form is submitted
-    this.onClick()
+    this.onClick();
   }
 
-  setLength(event){
-    this.charsRemaining = 500 - event.length
+  setLength(event) {
+    this.charsRemaining = 500 - event.length;
   }
 
-  getCourses(){
+  getCourses() {
     //this loads the spinner while it loads
-    this.coursesService.setStatus("loading")
-    this.coursesService.fetchEvent.emit(this.coursesService.getStatus())
+    this.coursesService.setStatus('loading');
+    this.coursesService.fetchEvent.emit(this.coursesService.getStatus());
     setTimeout(
-      () => this.coursesService.fetchAllCourses().subscribe((r)=>{
-        //set coursesArray in coursesService to r
-        this.coursesService.setCourses(r)
-        // setCoursesArray in this component to be the same as in the service
-        this.coursesArray = this.coursesService.getCourses()
-        //set status to success to stop rendering the spinner and load the component
-        this.coursesService.setStatus("success")
-        this.coursesService.fetchEvent.emit(this.coursesService.getStatus())
-        console.log("Fetched")
-      }), 1000)
+      () =>
+        this.coursesService.fetchAllCourses().subscribe((r) => {
+          //set coursesArray in coursesService to r
+          this.coursesService.setCourses(r);
+          // setCoursesArray in this component to be the same as in the service
+          this.coursesArray = this.coursesService.getCourses();
+          //set status to success to stop rendering the spinner and load the component
+          this.coursesService.setStatus('success');
+          this.coursesService.fetchEvent.emit(this.coursesService.getStatus());
+          console.log('Fetched');
+        }),
+      1000
+    );
   }
-
 }
