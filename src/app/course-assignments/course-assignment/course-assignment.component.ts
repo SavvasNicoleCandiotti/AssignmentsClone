69b42b8c6b127e77766d5d25c4a9c6b181496379
SavvasNicoleCandiotti <nicole.candiotti@savvas.com>
2022-07-11 15:45:57 -0400

    add kebab and number randomizer

--- src/app/course-assignments/course-assignment/course-assignment.component.ts
+++ src/app/course-assignments/course-assignment/course-assignment.component.ts
@@ -2,28 +2,35 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CourseAssignmentsService } from '../../services/course-assignments.service';
 import { CourseAssignmentInterface } from '../../models/CourseAssignmentInterface';
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-course-assignment',
   templateUrl: './course-assignment.component.html',
-  styleUrls: ['./course-assignment.component.css']
+  styleUrls: ['./course-assignment.component.css'],
 })
 export class CourseAssignmentComponent implements OnInit {
-  @Input() courseAssignment : CourseAssignmentInterface
+  @Input() courseAssignment: CourseAssignmentInterface;
+  faEllipsisVertical = faEllipsisVertical;
+  iconNumber: number = this.randomNumber();
 
   constructor(
-    private courseAssignmentsService : CourseAssignmentsService,
-    private router : Router,
-    private route : ActivatedRoute
-  ) {
-   }
+    private courseAssignmentsService: CourseAssignmentsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  randomNumber(): number {
+    return Math.floor(Math.random() * 8) + 1;
   }
 
   selectCourseAssignment = () => {
-    this.courseAssignmentsService.selectCourseAssignmentEvent.emit(this.courseAssignment)
-    this.router.navigate([this.courseAssignment.id], {relativeTo: this.route})
-  }
-
+    this.courseAssignmentsService.selectCourseAssignmentEvent.emit(
+      this.courseAssignment
+    );
+    this.router.navigate([this.courseAssignment.id], {
+      relativeTo: this.route,
+    });
+  };
 }
