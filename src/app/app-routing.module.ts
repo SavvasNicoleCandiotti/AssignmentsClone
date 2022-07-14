68bc2d397b1007dfe5d2fe614beab57944c6374c
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-07-14 16:21:26 -0500

    Update programs path to use nested routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
-import { identifierName } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AssignmentDetailComponent } from './assignments/assignment-detail/assignment-detail.component';
-import { AssignmentsComponent } from './assignments/assignments.component';
 import { CourseAssignmentDetailComponent } from './course-assignments/course-assignment-detail/course-assignment-detail.component';
 import { CourseAssignmentsComponent } from './course-assignments/course-assignments.component';
 import { HomeComponent } from './home/home.component';
+import { ProgramAssignmentDetailComponent } from './program-assignments/program-assignment-detail/program-assignment-detail.component';
+import { ProgramAssignmentsComponent } from './program-assignments/program-assignments.component';
 import { ProgramDetailComponent } from './programs/program-detail/program-detail.component';
 import { ProgramsComponent } from './programs/programs.component';
 
@@ -13,19 +12,12 @@ const routes: Routes = [
   {
     path: "programs",
     children: [
-      {
-        path: ":id", 
+      { path: "", component: ProgramsComponent },
+      { path: ":id", component: ProgramDetailComponent,
         children: [
-          { path: "", component: ProgramDetailComponent},
-          {
-            path: "assignments", 
-            children: [
-              {path: ":id", component: AssignmentDetailComponent},
-              {path: '', component: AssignmentsComponent}
-            ]}
+          { path: "assignments/:id",  component: ProgramAssignmentDetailComponent },
         ]
-      },
-      {path: "", component: ProgramsComponent}
+      }
     ]
   },
   {
