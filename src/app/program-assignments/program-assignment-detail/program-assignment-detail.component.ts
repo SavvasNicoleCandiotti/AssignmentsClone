e6272e73f1f42c138a196cad5d61baf6c3970de3
SavvasJeremiahSnidar <107058340+SavvasJeremiahSnidar@users.noreply.github.com>
2022-07-14 16:17:48 -0500

    Update to fetch data if needed

--- src/app/program-assignments/program-assignment-detail/program-assignment-detail.component.ts
+++ src/app/program-assignments/program-assignment-detail/program-assignment-detail.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AssignmentInterface } from 'src/app/models/AssignmentInterface';
 import { AssignmentsService } from 'src/app/services/assignments.service';
 import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 import { CourseAssignmentsService } from 'src/app/services/course-assignments.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-program-assignment-detail',
@@ -13,16 +14,35 @@ export class ProgramAssignmentDetailComponent implements OnInit {
   faEllipsisVertical=faEllipsisVertical
 
 
-  @Input() programAssignment : AssignmentInterface
+  programAssignment : AssignmentInterface
+  status : string = 'idle'
 
   constructor(
     private assignmentsService: AssignmentsService,
-    private courseAssignmentsService : CourseAssignmentsService
+    private courseAssignmentsService : CourseAssignmentsService,
+    private route : ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.assignmentsService.selectProgramAssignmentEvent
-    .subscribe(programAssignment => this.programAssignment = programAssignment)
+
+    const id = +this.route.snapshot.params['id']
+    
+    if(this.assignmentsService.getAssignment(id)){
+      this.programAssignment = this.assignmentsService.getAssignment(id)
+    }else{
+      this.getFetchedProgramAssignment()
+    }
+
+    this.assignmentsService.fetchEvent
+      .subscribe(status => this.status = status);
+
+    this.route.params.subscribe((params : Params) => {
+      if(this.assignmentsService.getAssignment(+params['id'])){
+        this.programAssignment = this.assignmentsService.getAssignment(+params['id'])
+      }else{
+        this.getFetchedProgramAssignment()
+      }
+    })
   }
 
   openCreateCourseAssignmentModal(){
@@ -31,6 +51,20 @@ export class ProgramAssignmentDetailComponent implements OnInit {
 
   closeModal(){
     this.courseAssignmentsService.toggleModalEvent.emit(false) 
+  }
+
+  getFetchedProgramAssignment(){
+    this.assignmentsService.setAssignmentStatus("loading")
+    this.assignmentsService.fetchEvent.emit("loading")
+    setTimeout(
+      () => this.assignmentsService.fetchAssignment(+this.route.snapshot.params['id']).subscribe((r)=>{
+        this.assignmentsService.addAssignment(r)
+        this.programAssignment = this.assignmentsService.getAssignment(+this.route.snapshot.params['id'])
+        this.assignmentsService.setAssignmentStatus("success")
+        this.assignmentsService.fetchEvent.emit("success")
+
+        console.log("fetched")
+      }), 1000)
   }
 
 }
